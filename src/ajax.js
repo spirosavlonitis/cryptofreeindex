c77c1b476a6f3a4e2571b0adc6c01bd12ea49c09
Spiros Avlonitis <spirosa84@hotmail.com>
2018-10-30 22:21:57 +0200

    dashboard upgrade

--- src/ajax.js
+++ src/ajax.js
@@ -5,7 +5,22 @@ class Ajax {
 				"BTC","LTC", "BCH", "ETH", "ETC", "ZEC", "DASH","XMR", "DCR",
 			];
 		this.curr_coin = document.getElementsByClassName('active')[0].childNodes[0].innerText;
-		
+		this.url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms="+this.coins.join()+"&tsyms="+this.curr_coin;
+	}
+
+	request() {
+		let xhttp = new XMLHttpRequest();
+		xhttp.open('GET', this.url, true);
+		xhttp.curr_coin = this.curr_coin;
+		xhttp.onreadystatechange = function()  {
+				if (this.readyState === 4 && this.status === 200) {
+					let response = JSON.parse(this.responseText);
+ 					for (let coin in response)
+							document.getElementById(this.curr_coin+'_'+coin
+							).innerHTML = response[coin].USD;
+				}
+		}
+		xhttp.send();
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -9,11 +9,18 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render([<Top />, <App />, <Consent />], document.getElementById('root'));
 
-
 if (module.hot)
 	module.hot.accept();
 
 serviceWorker.unregister();
 
 const ajax = new Ajax();
-console.log(ajax.curr_coin);
+let dashBoardTimer = undefined;
+
+if (typeof dashBoardTimer != 'undefined') {
+	clearInterval(dashBoardTimer);
+	dashBoardTimer = undefined;
+}
+
+if (typeof dashBoardTimer == 'undefined')
+	dashBoardTimer = setInterval( function(){ ajax.request() }, 30000);
