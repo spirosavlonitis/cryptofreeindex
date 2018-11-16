6b0213e020bc189e2b40b0534b78c32d75246869
Spiros Avlonitis <spirosa84@hotmail.com>
2018-11-16 06:59:40 +0200

    first working

--- src/Statistics/index.js
+++ src/Statistics/index.js
@@ -11,12 +11,13 @@ class Statistics extends Component {
 
   constructor(props) {
     super(props);
-    this.coins = [
-      {id: "BTC", color: "#f2a900"}, {id: "LTC", color: "#d3d3d3"}, {id: "BCH", color: "#4cca47"},
-      {id: "ETH", color: "#3385ff"}, {id: "ETC", color: "#669073"}, {id: "ZEC", color: "#f4b728"},
-      {id: "DASH", color: "#2075bc"}, {id: "XMR", color: "#ff6600"}, {id: "DCR", color: "#62D0C9"},
-    ];
-    this.navCoins = [ "USD", "EUR", "GBP",].concat(this.coins.map(coin => coin.id));
+    this.coins = {
+      BTC: {color: "#f2a900"}, LTC: {color: "#d3d3d3"}, BCH: {color: "#4cca47"},
+      ETH: {color: "#3385ff"}, ETC: {color: "#669073"}, ZEC: {color: "#f4b728"},
+      DASH:{ color: "#2075bc"}, XMR: {color: "#ff6600"}, DCR: {color: "#62D0C9"},
+    };
+    this.navCoins = [ "USD", "EUR", "GBP",].concat((Object.keys(this.coins)));
+    console.log(this.navCoins)
     this.currentCoin = document.getElementsByClassName('active')[0].childNodes[0].innerText;
     this.state = {
       chart: undefined,
@@ -26,7 +27,7 @@ class Statistics extends Component {
     window.addEventListener('mousedown', this.coinChanged.bind(this));
   }
   
-  chartSettings(chart, coins) {
+  chartSettings(chart, coin) {
     chart.paddingRight = 20;
     let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
     dateAxis.renderer.grid.template.location = 0;
@@ -37,21 +38,18 @@ class Statistics extends Component {
 
     let scrollbarX = new am4charts.XYChartScrollbar();
     /* add coin to chart and scrobarX series  */
-    for (let i in coins) {
-      let series = chart.series.push(new am4charts.LineSeries());
-      series.name = coins[i].id;
-      series.dataFields.dateX = "date";
-      series.dataFields.valueY = coins[i].id;
-      series.fill = am4core.color(coins[i].color);
-      series.stroke = am4core.color(coins[i].color);
-      series.tooltipText = "{valueY.value}";
+    let series = chart.series.push(new am4charts.LineSeries());
+    series.name = coin;
+    series.dataFields.dateX = "date";
+    series.dataFields.valueY = coin;
+    series.fill = am4core.color(this.coins[coin].color);
+    series.stroke = am4core.color(this.coins[coin].color);
+    series.tooltipText = "{valueY.value}";
   
-      scrollbarX.series.push(series);
-    }
+    scrollbarX.series.push(series);
 
     chart.cursor = new am4charts.XYCursor();
     chart.scrollbarX = scrollbarX;
-
     chart.legend = new am4charts.Legend();
     chart.legend.parent = chart.plotContainer;
     chart.legend.zIndex = 100;
@@ -60,50 +58,35 @@ class Statistics extends Component {
     valueAxis.renderer.grid.template.strokeOpacity = 0.07;
   }
 
-  setChart(days=365) {
-    let urls = []
-    for (var i = 0; i < this.coins.length; i++)
-      if(this.navCoins.slice(0,3).includes(this.currentCoin))
-        urls[i] = "https://min-api.cryptocompare.com/data/histoday?fsym="+this.coins[i].id+"&tsym="+this.currentCoin+"&limit="+days+"&aggregate=1&e=CCCAGG";
-      else
-        urls[i] = "https://min-api.cryptocompare.com/data/histoday?fsym="+this.currentCoin+"&tsym="+this.coins[i].id+"&limit="+days+"&aggregate=1&e=CCCAGG";
+  setChart(coin, days=365) {
+    let url;
+    console.log(this.coins[coin])
+    if(this.navCoins.slice(0,3).includes(this.currentCoin))
+      url = "https://min-api.cryptocompare.com/data/histoday?fsym="+coin+"&tsym="+this.currentCoin+"&limit="+days+"&aggregate=1&e=CCCAGG";
+    else
+      url = "https://min-api.cryptocompare.com/data/histoday?fsym="+this.currentCoin+"&tsym="+coin+"&limit="+days+"&aggregate=1&e=CCCAGG";
     this.setState({isLoading: true});
-    axios.all([
-        axios.get(urls[0]), axios.get(urls[1]), axios.get(urls[2]),
-        axios.get(urls[3]), axios.get(urls[4]), axios.get(urls[5]),
-        axios.get(urls[6]), axios.get(urls[7]), axios.get(urls[8]),
-      ])
-      .then(axios.spread( (btcRes, ltcRes, bchRes, ethRes, etcRes, zecRes, dashRes, xmrRes, dcrRes) => {
+    axios.get(url)
+      .then(res => {
           let chart = am4core.create("chartdiv", am4charts.XYChart);
-          this.chartSettings(chart, this.coins);          
-          
-          let results = [btcRes, ltcRes, bchRes, ethRes, etcRes, zecRes, dashRes, xmrRes, dcrRes];
-          let dateRes = undefined;
+          this.chartSettings(chart, coin);          
           chart.data = [];
           try {
-            for (let i in results)
-              if (results[i].data.Response !== "Error") {
-                dateRes = results[i];
-                break;
-              }
-              for (var i = 0; i < days+1; i++) {
+              for (var i = 0; i < days+1; i++)
                 chart.data[i] = {
-                  date: new Date(dateRes.data.Data[i].time*1000),
+                  date: new Date(res.data.Data[i].time*1000),
+                  [coin]: res.data.Data[i].close         
                 };
-                for (var j = 0; j < results.length; j++) 
-                  chart.data[i][this.coins[j].id] = ((results[j].data.Response === "Error") ?  1 : results[j].data.Data[i].close);
-              };
           }catch(e) {
             alert("Error while getting chart data!!");
             chart.data = [];
           }
           this.setState({ chart, isLoading: null });
         })
-    )
   }
 
   componentDidMount() {
-    this.setChart(365);
+    this.setChart('BTC');
   }
 
   componentCleanup() {
